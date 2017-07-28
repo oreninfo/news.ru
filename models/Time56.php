fdcef8ff79bd117f8311c46ccca8c3e6e6ebfb72
boris.akimov <oreninfo@mail.ru>
2017-07-28 10:52:33 +0500

    делаем админку

--- models/Time56.php
+++ models/Time56.php
@@ -6,15 +6,27 @@ use yii\db\ActiveRecord;
 
 
 class Time56 extends ActiveRecord
-{
-	public static function tableName()
+{	
+        public function rules()
+        {
+            return [
+                [['title','content'], 'required'],
+            ];
+        }
+        public static function tableName()
 	{
 		return 'time56';
 	}
-	
-	public static function getAll()
+        public static function getAll()
 	{
 		$data = self::find()->all();
+		return $data;
+	}
+        public static function getOne($id)
+	{
+		$data = self::find()
+                        ->where(['id'=>$id])
+                        ->one();
 		return $data;
 	}
 	
