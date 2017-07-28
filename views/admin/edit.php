cfdd0c2d3a5dcbd0a21e9fe7c8ea2889a2260752
boris.akimov <oreninfo@mail.ru>
2017-07-28 10:39:51 +0500

    делаем админку

---
+++ views/admin/edit.php
@@ -0,0 +1,19 @@
+<?php
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+?>
+<h1>Редактировать</h1>
+
+<?php $form = ActiveForm::begin();?>
+<div class="row">
+    <div class="col-md-6">
+        <?= $form->field($one, 'title')->textInput() ?>
+    </div>
+    <div class="col-md-6">
+        <?= $form->field($one, 'content')->textInput() ?>
+    </div>
+    <div class="col-md-12">
+        <?= Html::submitButton('Изменить', ['class' => 'btn btn-success'])?>
+    </div>
+</div>
+<?php ActiveForm::end();?>
--- views/admin/index.php
+++ views/admin/index.php
@@ -3,12 +3,13 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 ?>
 <h1>Админка</h1>
-<a href="/controllers/admin/" class="btn btn-primary">Создать</a>
+<a href="/admin/create" class="btn btn-primary">Создать</a>
 <table class="table">
     <thead>
         <tr>
             <td>#</td>
             <td>Название</td>
+            <td>Дата создания</td>
             <td>Действия</td>
         </tr>
     </thead>
@@ -17,9 +18,10 @@ use yii\widgets\ActiveForm;
         <tr>
             <td><?=$item->id?></td>
             <td><?=$item->title?></td>
+            <td><?=$item->created_at?></td>
             <td>
-                <a href="/views/admin/edit/<?=$item->id?>">Редактировать</a>
-                <a href="/views/admin/delete/<?=$item->id?>">Удалить</a>
+                <a href="/admin/edit<?=$item->id?>">Редактировать</a>
+                <a href="/admin/delete<?=$item->id?>">Удалить</a>
             </td>
         </tr>
         <?php endforeach;?>
