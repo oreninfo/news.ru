0c99cf03d4f7e2b219b7e12d731799bbc33b993e
boris.akimov <oreninfo@mail.ru>
2017-08-30 16:41:32 +0500

    дополняю

---
+++ views/test/index.php
@@ -0,0 +1,14 @@
+<?php 
+use yii\widgets\ActiveForm;
+?>
+ 
+<?php $form = ActiveForm::begin([
+    'options' => ['enctype' => 'multipart/form-data']
+    ]); 
+?>
+ 
+<?= $form->field($model, 'file')->fileInput(); ?>
+ 
+<button>Submit</button>
+ 
+<?php ActiveForm::end(); ?>
