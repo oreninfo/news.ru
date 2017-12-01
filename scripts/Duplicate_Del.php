79d1791ed5df0add83779f73efc51aa04624c575
boris.akimov <@PC>
2017-12-01 16:46:55 +0500

    попрабил таблицу time56 и удалил поле id_category

--- scripts/Duplicate_Del.php
+++ scripts/Duplicate_Del.php
@@ -1,39 +1,22 @@
 <?php
 $db = mysql_connect("localhost", "stas", "123456") or die ("MySQL сервер недоступен!" .mysql_error());
 mysql_select_db("link_bd", $db) or die ("Не удалось подключиться к базе даных!”" .mysql_error());
-$hQuery = mysql_query("SELECT id, title FROM time56 WHERE title IN (SELECT title FROM time56 GROUP BY title HAVING COUNT(title)>1);");
+$hQuery = mysql_query("SELECT id, title, content_path FROM time56 WHERE title IN (SELECT title FROM time56 GROUP BY title HAVING COUNT(title)>1);");
 while($aResult = mysql_fetch_assoc($hQuery))
 {
    $aMas[] = $aResult;
 }
-echo $aMas[17]['id'];
-/*while($aResult = mysql_fetch_assoc($hQuery)){
- $aNews[] = $aResult;
-}
-
-for ($i = 0 ; $i < count($aNews); ++$i)
-   {    $V=$aNews[$i][id].",".$aNews[$i][id_category];
-        $aValuesIns[]="($V)";
-        for ($j = $i+1; $j<count($aNews);++$j){
-            if ($aNews[$i][title] === $aNews[$j][title] and $aNews[$i][image_path] === $aNews[$j][image_path]) {
-                $V=$aNews[$i][id].",".$aNews[$j][id_category];
-                $aValuesIns[]="($V)";
-                /*if( isset($aNews[$j])  )
-                    {
-                    unset( $aNews[$j] );
-                    
-                    }
-               // --$j;
-            } 
+For($i = 0;$i<=count($aMas);$i++)
+{
+    For($j = $i+1;$j<=count($aMas);$j++)
+    {
+        If(strcmp($aMas[$i]['title'], $aMas[$j]['title']) == 0 AND strcmp($aMas[$i]['content_path'], $aMas[$j]['content_path']) == 0)
+        {
+            echo $aMas[$i]['id']." == ".$aMas[$j]['id']."\r\n";
+            $a = $aMas[$i]['id'];
+            $b = $aMas[$j]['id'];
+            $hQuery = mysql_query("DELETE FROM time56 WHERE id='$b';");
+            $hQuery = mysql_query("UPDATE time56_category SET news_id='$a' WHERE news_id='$b';");
         }
-    } 
-    print_rе ($aValuesIns);
- 	$aValuesIns = implode("," ,$aValuesIns);
-	$Query = "INSERT INTO `time56_category` (`news_id`, `category_id`) VALUES $aValuesIns;";
-	$hQuery = mysql_query($Query);
-	if ($hQuery === TRUE) {
-		echo "Успешно";
-	}
-	else {
-	       echo "Не успешно";
-	} */
+    }
+}
